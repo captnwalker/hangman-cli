2d937871abb417310e8a35a72d09feae2b547b93
Steve Walker <captnwalker@gmail.com>
2018-01-09 16:55:43 -0500

    build hangman game display

--- hangman.js
+++ hangman.js
@@ -43,25 +43,35 @@ figlet('Welcome to ', function (err, data) {
         });
     });
 });
-//Vars - set permitted letters and arrays for guesses
+//Vars - set permitted letters, arrays for guesses and display
 var letter = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 var guessed = [];
 var correctGuess = [];
-
+var letterDisplay;
+//Set game var
 var game = {
-    //fetch words, set # of guesses, start null
+    //Fetch words, set # of guesses, start null
     wordBank: dictionary,
     guessesRemaining: 10,
-    currentWrd: null,
+    currentWord: null,
 
 
     setupGame: function () {
-        //initial guess count
-        this.guessesRemaining = 10;
+        //Initial guess count
+        this.remainingGuesses = 10;
 
-        //generate random POTUS name
+        //Generate random POTUS name
         var pres = Math.floor(Math.random() * this.dictionary.length);
         this.currentWord = this.dictionary[pres];
 
+        //Display letters placeholders ( _ _ _ _ _ ) and any correctly guessed letters here
+        letterDisplay = new displayLetters(this.currentWord);
+        letterDisplay.parseDisplay();
+        console.log("Guesses Remaining: " + game.remainingGuesses);
+
+        //Prompt for another letter
+        keepPromptingUser();
     }
-}
+
+
+};
