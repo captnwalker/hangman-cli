95a872100a6bc7a18fd13c8c818ce934a9ecb687
Steve Walker <captnwalker@gmail.com>
2018-01-09 20:21:12 -0500

    changes to hm.js

--- hangman.js
+++ hangman.js
@@ -13,6 +13,8 @@ var displayLetter = require('./letter.js');
 
 var playersGuess = require('./word.js');
 
+var playersGuess = require('./dictionary.js');
+
 //Game Splash Intro Screen and instructions
 figlet('Welcome to ', function (err, data) {
     if (err) {
@@ -60,11 +62,11 @@ var game = {
         //Initial guess count
         this.remainingGuesses = 10;
 
-        //Generate random POTUS name
+        //Generate random POTUS name from dictionary.js
         var pres = Math.floor(Math.random() * this.dictionary.length);
         this.currentWord = this.dictionary[pres];
 
-        //Display letters placeholders ( _ _ _ _ _ ) and any correctly guessed letters here
+        //Display letters placeholders and any correctly guessed letters here
         letterDisplay = new displayLetters(this.currentWord);
         letterDisplay.parseDisplay();
         console.log("Guesses Remaining: " + game.remainingGuesses);
